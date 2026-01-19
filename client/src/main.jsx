165c3dcd0e9e516d411c5070c8bd5f367a8bf9d6
koma123451 <daibowen6111@gmail.com>
2026-01-19 14:02:49 -0500

    fixed main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.jsx";
-const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
-  },
-});
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")).render(
-  <ChakraProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ChakraProvider>
+  <StrictMode>
+    <ChakraProvider resetCSS>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </StrictMode>
 );
