ef8ad8e91c46fdbaf120a6d8779da0fb3adc55a6
koma123451 <daibowen6111@gmail.com>
2026-01-19 13:52:18 -0500

    fixed: using extendTheme for UI errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,19 @@
-import "@chakra-ui/react/dist/style.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-import App from "./App.jsx";
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
-  </StrictMode>
+  <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
 );
