a8971e95c4075e4f306536e5b78a4197042a5c9f
koma123451 <daibowen6111@gmail.com>
2026-01-19 13:43:38 -0500

    fixed: Chakra UI config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import "@chakra-ui/react/dist/style.css";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <ChakraProvider>
+    <ChakraProvider>
       <BrowserRouter>
-      <App />
+        <App />
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
-  
-)
+);
