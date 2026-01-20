cc69ab338f222e97ac9f2145c5e12e6c1d350661
koma123451 <daibowen6111@gmail.com>
2026-01-20 09:15:11 -0500

    fixing UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
-
+import { ChakraProvider,ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ChakraProvider resetCSS>
+    <ChakraProvider theme={theme}>
+        <ColorModeScript
+        initialColorMode={theme.config.initialColorMode}
+      />
       <BrowserRouter>
         <App />
       </BrowserRouter>
